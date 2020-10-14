2aa62ab7c10783dceacb90e9df4c98010b93f573
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2020-10-14 22:04:44 +0300

    feat: add only 15 from 20 test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsConfigLeft = [];
+    let bracketsConfigRight = [];
+    let result = [];
+
+    for (bracket of bracketsConfig) {
+        bracketsConfigLeft.push(bracket[0]);
+        bracketsConfigRight.push(bracket[1]);
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        let item = str[i];
+
+        bracketsConfigRight.indexOf(item) > -1
+            ? result.pop()
+            : result.push(item);
+    }
+
+    return !result.length;
+};
